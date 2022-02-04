b5c6b5d0f465aca84f9d3ccc3026f4c2d2f127b2
Victor Dunaivits <victordunaivits@gmail.com>
2022-02-04 15:28:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let sec = 0;
+let min = 0;
+let hora = 0;
+let interval;
+
+function formatacao(valor) {
+  if (valor < 10) {
+    return "0" + valor;
+  } else {
+    return valor;
+  }
+}
+
+function start() {
+  interval = setInterval(function () {
+    sec++;
+
+    if (sec == 60) {
+      min++;
+      sec = 0;
+    }
+
+    if (min == 59) {
+      hora++;
+      min = 0;
+    }
+
+    document.getElementById("watch").innerText =
+      formatacao(hora) + ":" + formatacao(min) + ":" + formatacao(sec);
+  }, 1000);
+}
+
+function pause() {
+  clearInterval(interval);
+}
+
+function reset() {
+  clearInterval(interval);
+  sec = 0;
+  min = 0;
+  hora = 0;
+
+  document.getElementById("watch").innerText = "00:00:00";
+}
